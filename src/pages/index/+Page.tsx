a9ef7910c1f6170cb4eeba0df460f8e7359ff9db
Jihyun <hjh20030615@gmail.com>
2025-11-17 06:06:34 +0900

    feat: 검색창 컴포넌트 생성 완료

--- src/pages/index/+Page.tsx
+++ src/pages/index/+Page.tsx
@@ -2,6 +2,8 @@ import {usePopup} from '../../components/popup/usePopup';
 import Input from '../../components/input/Input';
 import Button from '../../components/button/Button';
 import {useInput} from '../../components/input/useInput';
+import SearchInput from '../../components/search/Search';
+import {useSearch} from '../../components/search/useSearch';
 
 interface ConfirmData {
   userName: string;
@@ -19,6 +21,15 @@ export default function Page() {
     // 인풋 예시입니다
     initialValue: '',
     validate: value => (value.length > 200 ? '200자 이내로 입력해주세요.' : null),
+  });
+
+  const search = useSearch({
+    // 검색창 예시입니다
+    initialKeyword: '',
+    async onSearch(keyword) {
+      console.log('검색 실행:', keyword);
+      // 여기서 API 호출 같은 거 하면 됨
+    },
   });
 
   return (
@@ -53,6 +64,12 @@ export default function Page() {
         placeholder="자기소개를 입력하세요"
         className="px-4 py-3 text-sm min-h-[120px]"
         {...bioInput.bind}
+      />
+
+      <SearchInput
+        wrapperClassName="mb-4"
+        inputClassName="text-[13px]" // 필요하면 여기서 글자 크기/패딩 조절
+        {...search.bindInput}
       />
 
       {/* Modal  */}
