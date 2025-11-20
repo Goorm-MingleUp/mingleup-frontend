416bdcf478b589c34c7cc54cc5dc720d12e087c0
Jihyun <hjh20030615@gmail.com>
2025-11-20 17:34:04 +0900

    feat: dropdown 컴포넌트 완성

--- src/pages/index/+Page.tsx
+++ src/pages/index/+Page.tsx
@@ -1,4 +1,15 @@
+import Dropdown from '../../components/dropdown/Dropdown';
+import {useDropdown} from '../../components/dropdown/useDropdown';
+
+const OPTIONS = [
+  {label: '옵션 1', value: 'opt1'},
+  {label: '옵션 2', value: 'opt2'},
+  {label: '옵션 3', value: 'opt3'},
+];
+
 export default function Page() {
+  const dropdown = useDropdown();
+
   return (
     <>
       <h1 className={'font-bold text-3xl pb-4'}>1주차 멘토링 과제</h1>
@@ -10,6 +21,15 @@ export default function Page() {
       {/* Modal  */}
 
       {/* Dropdown */}
+      <Dropdown
+        options={OPTIONS}
+        placeholder="옵션을 선택해주세요"
+        wrapperClassName="w-[240px]"
+        buttonClassName="w-full px-4 py-2"
+        optionListClassName="w-full"
+        optionClassName="w-full"
+        {...dropdown.bind}
+      />
     </>
   );
 }
