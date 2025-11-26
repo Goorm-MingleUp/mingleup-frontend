c9a0a5c0f7c639b90da9c51b83614d4e46299554
sonseongyeol <sonseong10@gmail.com>
2025-11-26 17:09:47 +0900

    feat(party-detail): markup page and base style

--- src/pages/party/+Page.tsx
+++ src/pages/party/+Page.tsx
@@ -5,20 +5,24 @@ const PartyList = lazy(() => import('./components/PartyList'));
 function PartyPage() {
   return (
     <>
-      <div className="pt-6 flex justify-between mb-10">
-        <h2 className="text-4xl">파티리스트</h2>
-        <SearchInput />
+      <div className="container">
+        <div className="pt-6 flex justify-between mb-10 ">
+          <h2 className="text-4xl">파티리스트</h2>
+          <SearchInput />
+        </div>
       </div>
 
-      <div className="mb-12">
-        <ul className="flex items-center gap-3">
-          <li className="px-5 py-2.5 rounded-4xl border bg-black text-white text-center ">전체</li>
-          <li className="px-5 py-2.5 rounded-4xl border text-center ">마감 임박</li>
-          <li className="px-5 py-2.5 rounded-4xl border text-center ">MBTI</li>
-        </ul>
+      <div className="container">
+        <div className="mb-12">
+          <ul className="flex items-center gap-3">
+            <li className="px-5 py-2.5 rounded-4xl border bg-black text-white text-center ">전체</li>
+            <li className="px-5 py-2.5 rounded-4xl border text-center ">마감 임박</li>
+            <li className="px-5 py-2.5 rounded-4xl border text-center ">MBTI</li>
+          </ul>
+        </div>
       </div>
 
-      <section>
+      <section className="container">
         <Suspense fallback={<p>로딩중입니다.</p>}>
           <PartyList />
         </Suspense>
